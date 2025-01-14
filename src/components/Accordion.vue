2980b4f0629cac3a00158d907378b0ecf8855d44
geeksaider <nikita.bondarev.06@gmail.com>
2025-01-14 19:00:25 +0300

    imstupid10000000

---
+++ src/components/Accordion.vue
@@ -0,0 +1,31 @@
+<script setup>
+const { title, links } = defineProps(["title", "links"]);
+</script>
+
+<template>
+    <details
+        class="bg-stone-100 rounded-b-md group transition-all duration-500"
+    >
+        <summary
+            class="bg-stone-200 px-8 py-2 font-semibold text-xl rounded-md cursor-pointer list-none flex items-center gap-4 mb-0 transition-all duration-300 group-open:mb-4 ease-out"
+        >
+            <img
+                src="/arrow.svg"
+                alt=""
+                class="bg-white p-1.5 rounded-full group-open:-rotate-180 duration-300 transition-transform ease-out"
+            />
+            <p>{{ title }}</p>
+        </summary>
+        <div
+            class="flex flex-col px-8 gap-2 -mb-16 group-open:mb-0 transition-all ease-out duration-300"
+        >
+            <a
+                :href="item.link"
+                v-for="item in links"
+                target="_blank"
+                class="last:mb-4"
+                >{{ item.title }}</a
+            >
+        </div>
+    </details>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,21 +1,126 @@
 <script setup>
+import Accordion from "@/components/Accordion.vue";
 import Header from "@/components/Header.vue";
+import Arrow from "@/components/icons/Arrow.vue";
 </script>
 
 <template>
-  <main class="font-cute px-4">
-    <Header class="font-cute" />
-    <section class="flex justify-around">
-      <div>
-        <details>
-          <summary>Работы</summary>
-          <a href=""></a>
-        </details>
-      </div>
-      <div class="flex flex-col gap-4">
-        <img src="/assets/myface.jpeg" alt="" class="h-[600px] rounded-lg" />
-      </div>
-      <div>s</div>
-    </section>
-  </main>
+    <main class="font-cute px-4">
+        <Header class="font-cute" />
+        <section class="flex justify-around max-w-[900px] mx-auto">
+            <div class="flex flex-col gap-12">
+                <Accordion
+                    title="Работы"
+                    :links="[
+                        {
+                            title: 'Работа 1',
+                            link: 'https://www.google.com/imgres?q=%D1%81%D0%BE%D0%B1%D0%B0%D0%BA%D0%B0&imgurl=https%3A%2F%2Fcdnn21.img.ria.ru%2Fimages%2F07e5%2F05%2F11%2F1732647541_0%3A200%3A1920%3A1280_1920x0_80_0_0_dd4c418518310b9d20c8d8029948008a.jpg&imgrefurl=https%3A%2F%2Fria.ru%2F20210801%2Fsobaki-1743819444.html&docid=Ym0YDqlJQKqSgM&tbnid=ClTZeK4ctHHIQM&vet=12ahUKEwji0cXx4fOKAxU_JhAIHSt1MPIQM3oECGMQAA..i&w=1920&h=1080&hcb=2&ved=2ahUKEwji0cXx4fOKAxU_JhAIHSt1MPIQM3oECGMQAA',
+                        },
+                        {
+                            title: 'Работа 2',
+                            link: 'https://www.google.com/imgres?q=%D1%81%D0%BE%D0%B1%D0%B0%D0%BA%D0%B0&imgurl=https%3A%2F%2Fcdnn21.img.ria.ru%2Fimages%2F07e5%2F05%2F11%2F1732647541_0%3A200%3A1920%3A1280_1920x0_80_0_0_dd4c418518310b9d20c8d8029948008a.jpg&imgrefurl=https%3A%2F%2Fria.ru%2F20210801%2Fsobaki-1743819444.html&docid=Ym0YDqlJQKqSgM&tbnid=ClTZeK4ctHHIQM&vet=12ahUKEwji0cXx4fOKAxU_JhAIHSt1MPIQM3oECGMQAA..i&w=1920&h=1080&hcb=2&ved=2ahUKEwji0cXx4fOKAxU_JhAIHSt1MPIQM3oECGMQAA',
+                        },
+                        {
+                            title: 'Работа 3',
+                            link: 'https://www.google.com/imgres?q=%D1%81%D0%BE%D0%B1%D0%B0%D0%BA%D0%B0&imgurl=https%3A%2F%2Fcdnn21.img.ria.ru%2Fimages%2F07e5%2F05%2F11%2F1732647541_0%3A200%3A1920%3A1280_1920x0_80_0_0_dd4c418518310b9d20c8d8029948008a.jpg&imgrefurl=https%3A%2F%2Fria.ru%2F20210801%2Fsobaki-1743819444.html&docid=Ym0YDqlJQKqSgM&tbnid=ClTZeK4ctHHIQM&vet=12ahUKEwji0cXx4fOKAxU_JhAIHSt1MPIQM3oECGMQAA..i&w=1920&h=1080&hcb=2&ved=2ahUKEwji0cXx4fOKAxU_JhAIHSt1MPIQM3oECGMQAA',
+                        },
+                    ]"
+                />
+                <Accordion
+                    title="Работы"
+                    :links="[
+                        {
+                            title: 'Работа 1',
+                            link: 'https://www.google.com/imgres?q=%D1%81%D0%BE%D0%B1%D0%B0%D0%BA%D0%B0&imgurl=https%3A%2F%2Fcdnn21.img.ria.ru%2Fimages%2F07e5%2F05%2F11%2F1732647541_0%3A200%3A1920%3A1280_1920x0_80_0_0_dd4c418518310b9d20c8d8029948008a.jpg&imgrefurl=https%3A%2F%2Fria.ru%2F20210801%2Fsobaki-1743819444.html&docid=Ym0YDqlJQKqSgM&tbnid=ClTZeK4ctHHIQM&vet=12ahUKEwji0cXx4fOKAxU_JhAIHSt1MPIQM3oECGMQAA..i&w=1920&h=1080&hcb=2&ved=2ahUKEwji0cXx4fOKAxU_JhAIHSt1MPIQM3oECGMQAA',
+                        },
+                        {
+                            title: 'Работа 2',
+                            link: 'https://www.google.com/imgres?q=%D1%81%D0%BE%D0%B1%D0%B0%D0%BA%D0%B0&imgurl=https%3A%2F%2Fcdnn21.img.ria.ru%2Fimages%2F07e5%2F05%2F11%2F1732647541_0%3A200%3A1920%3A1280_1920x0_80_0_0_dd4c418518310b9d20c8d8029948008a.jpg&imgrefurl=https%3A%2F%2Fria.ru%2F20210801%2Fsobaki-1743819444.html&docid=Ym0YDqlJQKqSgM&tbnid=ClTZeK4ctHHIQM&vet=12ahUKEwji0cXx4fOKAxU_JhAIHSt1MPIQM3oECGMQAA..i&w=1920&h=1080&hcb=2&ved=2ahUKEwji0cXx4fOKAxU_JhAIHSt1MPIQM3oECGMQAA',
+                        },
+                        {
+                            title: 'Работа 3',
+                            link: 'https://www.google.com/imgres?q=%D1%81%D0%BE%D0%B1%D0%B0%D0%BA%D0%B0&imgurl=https%3A%2F%2Fcdnn21.img.ria.ru%2Fimages%2F07e5%2F05%2F11%2F1732647541_0%3A200%3A1920%3A1280_1920x0_80_0_0_dd4c418518310b9d20c8d8029948008a.jpg&imgrefurl=https%3A%2F%2Fria.ru%2F20210801%2Fsobaki-1743819444.html&docid=Ym0YDqlJQKqSgM&tbnid=ClTZeK4ctHHIQM&vet=12ahUKEwji0cXx4fOKAxU_JhAIHSt1MPIQM3oECGMQAA..i&w=1920&h=1080&hcb=2&ved=2ahUKEwji0cXx4fOKAxU_JhAIHSt1MPIQM3oECGMQAA',
+                        },
+                    ]"
+                />
+                <Accordion
+                    title="Работы"
+                    :links="[
+                        {
+                            title: 'Работа 1',
+                            link: 'https://www.google.com/imgres?q=%D1%81%D0%BE%D0%B1%D0%B0%D0%BA%D0%B0&imgurl=https%3A%2F%2Fcdnn21.img.ria.ru%2Fimages%2F07e5%2F05%2F11%2F1732647541_0%3A200%3A1920%3A1280_1920x0_80_0_0_dd4c418518310b9d20c8d8029948008a.jpg&imgrefurl=https%3A%2F%2Fria.ru%2F20210801%2Fsobaki-1743819444.html&docid=Ym0YDqlJQKqSgM&tbnid=ClTZeK4ctHHIQM&vet=12ahUKEwji0cXx4fOKAxU_JhAIHSt1MPIQM3oECGMQAA..i&w=1920&h=1080&hcb=2&ved=2ahUKEwji0cXx4fOKAxU_JhAIHSt1MPIQM3oECGMQAA',
+                        },
+                        {
+                            title: 'Работа 2',
+                            link: 'https://www.google.com/imgres?q=%D1%81%D0%BE%D0%B1%D0%B0%D0%BA%D0%B0&imgurl=https%3A%2F%2Fcdnn21.img.ria.ru%2Fimages%2F07e5%2F05%2F11%2F1732647541_0%3A200%3A1920%3A1280_1920x0_80_0_0_dd4c418518310b9d20c8d8029948008a.jpg&imgrefurl=https%3A%2F%2Fria.ru%2F20210801%2Fsobaki-1743819444.html&docid=Ym0YDqlJQKqSgM&tbnid=ClTZeK4ctHHIQM&vet=12ahUKEwji0cXx4fOKAxU_JhAIHSt1MPIQM3oECGMQAA..i&w=1920&h=1080&hcb=2&ved=2ahUKEwji0cXx4fOKAxU_JhAIHSt1MPIQM3oECGMQAA',
+                        },
+                        {
+                            title: 'Работа 3',
+                            link: 'https://www.google.com/imgres?q=%D1%81%D0%BE%D0%B1%D0%B0%D0%BA%D0%B0&imgurl=https%3A%2F%2Fcdnn21.img.ria.ru%2Fimages%2F07e5%2F05%2F11%2F1732647541_0%3A200%3A1920%3A1280_1920x0_80_0_0_dd4c418518310b9d20c8d8029948008a.jpg&imgrefurl=https%3A%2F%2Fria.ru%2F20210801%2Fsobaki-1743819444.html&docid=Ym0YDqlJQKqSgM&tbnid=ClTZeK4ctHHIQM&vet=12ahUKEwji0cXx4fOKAxU_JhAIHSt1MPIQM3oECGMQAA..i&w=1920&h=1080&hcb=2&ved=2ahUKEwji0cXx4fOKAxU_JhAIHSt1MPIQM3oECGMQAA',
+                        },
+                    ]"
+                />
+            </div>
+            <div class="flex flex-col gap-4">
+                <img
+                    src="/assets/myface.jpeg"
+                    alt=""
+                    class="h-[60vh] rounded-lg"
+                />
+            </div>
+            <div class="flex flex-col gap-12">
+                <Accordion
+                    title="Работы"
+                    :links="[
+                        {
+                            title: 'Работа 1',
+                            link: 'https://www.google.com/imgres?q=%D1%81%D0%BE%D0%B1%D0%B0%D0%BA%D0%B0&imgurl=https%3A%2F%2Fcdnn21.img.ria.ru%2Fimages%2F07e5%2F05%2F11%2F1732647541_0%3A200%3A1920%3A1280_1920x0_80_0_0_dd4c418518310b9d20c8d8029948008a.jpg&imgrefurl=https%3A%2F%2Fria.ru%2F20210801%2Fsobaki-1743819444.html&docid=Ym0YDqlJQKqSgM&tbnid=ClTZeK4ctHHIQM&vet=12ahUKEwji0cXx4fOKAxU_JhAIHSt1MPIQM3oECGMQAA..i&w=1920&h=1080&hcb=2&ved=2ahUKEwji0cXx4fOKAxU_JhAIHSt1MPIQM3oECGMQAA',
+                        },
+                        {
+                            title: 'Работа 2',
+                            link: 'https://www.google.com/imgres?q=%D1%81%D0%BE%D0%B1%D0%B0%D0%BA%D0%B0&imgurl=https%3A%2F%2Fcdnn21.img.ria.ru%2Fimages%2F07e5%2F05%2F11%2F1732647541_0%3A200%3A1920%3A1280_1920x0_80_0_0_dd4c418518310b9d20c8d8029948008a.jpg&imgrefurl=https%3A%2F%2Fria.ru%2F20210801%2Fsobaki-1743819444.html&docid=Ym0YDqlJQKqSgM&tbnid=ClTZeK4ctHHIQM&vet=12ahUKEwji0cXx4fOKAxU_JhAIHSt1MPIQM3oECGMQAA..i&w=1920&h=1080&hcb=2&ved=2ahUKEwji0cXx4fOKAxU_JhAIHSt1MPIQM3oECGMQAA',
+                        },
+                        {
+                            title: 'Работа 3',
+                            link: 'https://www.google.com/imgres?q=%D1%81%D0%BE%D0%B1%D0%B0%D0%BA%D0%B0&imgurl=https%3A%2F%2Fcdnn21.img.ria.ru%2Fimages%2F07e5%2F05%2F11%2F1732647541_0%3A200%3A1920%3A1280_1920x0_80_0_0_dd4c418518310b9d20c8d8029948008a.jpg&imgrefurl=https%3A%2F%2Fria.ru%2F20210801%2Fsobaki-1743819444.html&docid=Ym0YDqlJQKqSgM&tbnid=ClTZeK4ctHHIQM&vet=12ahUKEwji0cXx4fOKAxU_JhAIHSt1MPIQM3oECGMQAA..i&w=1920&h=1080&hcb=2&ved=2ahUKEwji0cXx4fOKAxU_JhAIHSt1MPIQM3oECGMQAA',
+                        },
+                    ]"
+                />
+                <Accordion
+                    title="Работы"
+                    :links="[
+                        {
+                            title: 'Работа 1',
+                            link: 'https://www.google.com/imgres?q=%D1%81%D0%BE%D0%B1%D0%B0%D0%BA%D0%B0&imgurl=https%3A%2F%2Fcdnn21.img.ria.ru%2Fimages%2F07e5%2F05%2F11%2F1732647541_0%3A200%3A1920%3A1280_1920x0_80_0_0_dd4c418518310b9d20c8d8029948008a.jpg&imgrefurl=https%3A%2F%2Fria.ru%2F20210801%2Fsobaki-1743819444.html&docid=Ym0YDqlJQKqSgM&tbnid=ClTZeK4ctHHIQM&vet=12ahUKEwji0cXx4fOKAxU_JhAIHSt1MPIQM3oECGMQAA..i&w=1920&h=1080&hcb=2&ved=2ahUKEwji0cXx4fOKAxU_JhAIHSt1MPIQM3oECGMQAA',
+                        },
+                        {
+                            title: 'Работа 2',
+                            link: 'https://www.google.com/imgres?q=%D1%81%D0%BE%D0%B1%D0%B0%D0%BA%D0%B0&imgurl=https%3A%2F%2Fcdnn21.img.ria.ru%2Fimages%2F07e5%2F05%2F11%2F1732647541_0%3A200%3A1920%3A1280_1920x0_80_0_0_dd4c418518310b9d20c8d8029948008a.jpg&imgrefurl=https%3A%2F%2Fria.ru%2F20210801%2Fsobaki-1743819444.html&docid=Ym0YDqlJQKqSgM&tbnid=ClTZeK4ctHHIQM&vet=12ahUKEwji0cXx4fOKAxU_JhAIHSt1MPIQM3oECGMQAA..i&w=1920&h=1080&hcb=2&ved=2ahUKEwji0cXx4fOKAxU_JhAIHSt1MPIQM3oECGMQAA',
+                        },
+                        {
+                            title: 'Работа 3',
+                            link: 'https://www.google.com/imgres?q=%D1%81%D0%BE%D0%B1%D0%B0%D0%BA%D0%B0&imgurl=https%3A%2F%2Fcdnn21.img.ria.ru%2Fimages%2F07e5%2F05%2F11%2F1732647541_0%3A200%3A1920%3A1280_1920x0_80_0_0_dd4c418518310b9d20c8d8029948008a.jpg&imgrefurl=https%3A%2F%2Fria.ru%2F20210801%2Fsobaki-1743819444.html&docid=Ym0YDqlJQKqSgM&tbnid=ClTZeK4ctHHIQM&vet=12ahUKEwji0cXx4fOKAxU_JhAIHSt1MPIQM3oECGMQAA..i&w=1920&h=1080&hcb=2&ved=2ahUKEwji0cXx4fOKAxU_JhAIHSt1MPIQM3oECGMQAA',
+                        },
+                    ]"
+                />
+                <Accordion
+                    title="Работы"
+                    :links="[
+                        {
+                            title: 'Работа 1',
+                            link: 'https://www.google.com/imgres?q=%D1%81%D0%BE%D0%B1%D0%B0%D0%BA%D0%B0&imgurl=https%3A%2F%2Fcdnn21.img.ria.ru%2Fimages%2F07e5%2F05%2F11%2F1732647541_0%3A200%3A1920%3A1280_1920x0_80_0_0_dd4c418518310b9d20c8d8029948008a.jpg&imgrefurl=https%3A%2F%2Fria.ru%2F20210801%2Fsobaki-1743819444.html&docid=Ym0YDqlJQKqSgM&tbnid=ClTZeK4ctHHIQM&vet=12ahUKEwji0cXx4fOKAxU_JhAIHSt1MPIQM3oECGMQAA..i&w=1920&h=1080&hcb=2&ved=2ahUKEwji0cXx4fOKAxU_JhAIHSt1MPIQM3oECGMQAA',
+                        },
+                        {
+                            title: 'Работа 2',
+                            link: 'https://www.google.com/imgres?q=%D1%81%D0%BE%D0%B1%D0%B0%D0%BA%D0%B0&imgurl=https%3A%2F%2Fcdnn21.img.ria.ru%2Fimages%2F07e5%2F05%2F11%2F1732647541_0%3A200%3A1920%3A1280_1920x0_80_0_0_dd4c418518310b9d20c8d8029948008a.jpg&imgrefurl=https%3A%2F%2Fria.ru%2F20210801%2Fsobaki-1743819444.html&docid=Ym0YDqlJQKqSgM&tbnid=ClTZeK4ctHHIQM&vet=12ahUKEwji0cXx4fOKAxU_JhAIHSt1MPIQM3oECGMQAA..i&w=1920&h=1080&hcb=2&ved=2ahUKEwji0cXx4fOKAxU_JhAIHSt1MPIQM3oECGMQAA',
+                        },
+                        {
+                            title: 'Работа 3',
+                            link: 'https://www.google.com/imgres?q=%D1%81%D0%BE%D0%B1%D0%B0%D0%BA%D0%B0&imgurl=https%3A%2F%2Fcdnn21.img.ria.ru%2Fimages%2F07e5%2F05%2F11%2F1732647541_0%3A200%3A1920%3A1280_1920x0_80_0_0_dd4c418518310b9d20c8d8029948008a.jpg&imgrefurl=https%3A%2F%2Fria.ru%2F20210801%2Fsobaki-1743819444.html&docid=Ym0YDqlJQKqSgM&tbnid=ClTZeK4ctHHIQM&vet=12ahUKEwji0cXx4fOKAxU_JhAIHSt1MPIQM3oECGMQAA..i&w=1920&h=1080&hcb=2&ved=2ahUKEwji0cXx4fOKAxU_JhAIHSt1MPIQM3oECGMQAA',
+                        },
+                    ]"
+                />
+            </div>
+        </section>
+    </main>
 </template>
