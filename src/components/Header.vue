b4c8ccd9a1e31b0279ef04792180278c09edb816
geeksaider <132765681+geeksaider@users.noreply.github.com>
2025-01-16 12:43:15 +0000

    test

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@ import Logo from "./icons/Logo.vue";
 </script>
 <template>
   <header
-    class="bg-gradient-to-r from-neutral-100 to-neutral-200 max-sm:flex-col max-sm:text-center max-w-[900px] max-sm:gap-4 mx-auto flex justify-between rounded-xl py-4 px-20 items-center my-12"
+    class="bg-gradient-to-r from-neutral-100 to-neutral-200 max-sm:flex-col max-sm:text-center max-w-[900px] max-sm:gap-4 flex justify-between rounded-xl py-4 px-20 items-center my-12"
   >
     <Logo></Logo>
     <h1 class="text-3xl font-bold">Украшение интерьера</h1>
