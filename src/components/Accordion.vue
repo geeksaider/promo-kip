013c20541b3f43892a8235d01c79e4208e3eb4d2
geeksaider <132765681+geeksaider@users.noreply.github.com>
2025-01-16 06:10:56 +0000

    fix accordion animation

--- src/components/Accordion.vue
+++ src/components/Accordion.vue
@@ -3,29 +3,28 @@ const { title, links } = defineProps(["title", "links"]);
 </script>
 
 <template>
-    <details
-        class="bg-stone-100 rounded-b-md group transition-all duration-500"
+  <details
+    class="bg-stone-100 rounded-b-md group transition-all pb-0 open:pb-4 duration-500"
+    name="accordiom"
+  >
+    <summary
+      class="bg-stone-200 px-8 py-2 font-semibold text-xl rounded-md cursor-pointer list-none flex items-center gap-4 mb-0 transition-all duration-300 group-open:mb-3 ease-out"
     >
-        <summary
-            class="bg-stone-200 px-8 py-2 font-semibold text-xl rounded-md cursor-pointer list-none flex items-center gap-4 mb-0 transition-all duration-300 group-open:mb-4 ease-out"
-        >
-            <img
-                src="/arrow.svg"
-                alt=""
-                class="bg-white p-1.5 rounded-full group-open:-rotate-180 duration-300 transition-transform ease-out"
-            />
-            <p>{{ title }}</p>
-        </summary>
-        <div
-            class="flex flex-col px-8 gap-2 -mb-16 group-open:mb-0 transition-all ease-out duration-300"
-        >
-            <a
-                :href="item.link"
-                v-for="item in links"
-                target="_blank"
-                class="last:mb-4"
-                >{{ item.title }}</a
-            >
-        </div>
-    </details>
+      <img
+        src="/arrow.svg"
+        alt=""
+        class="bg-white p-1.5 rounded-full group-open:-rotate-180 duration-300 transition-transform ease-out"
+      />
+      <p>{{ title }}</p>
+    </summary>
+    <div class="flex flex-col gap-2 px-6 transition-all ease-out duration-300">
+      <a
+        :href="item.link"
+        v-for="item in links"
+        target="_blank"
+        class="hover:text-stone-600"
+        >{{ item.title }}</a
+      >
+    </div>
+  </details>
 </template>
