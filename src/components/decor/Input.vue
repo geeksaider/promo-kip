08f407c040ec382fb9a43338722d2b4581b47ba2
geeksaider <132765681+geeksaider@users.noreply.github.com>
2025-01-29 10:42:01 +0000

    added input animation and

--- src/components/decor/Input.vue
+++ src/components/decor/Input.vue
@@ -1,4 +1,32 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import { defineProps } from "vue";
+
+const {
+  name = "",
+  type = "text",
+  disabled = false,
+} = defineProps(["name", "type", "disabled"]);
+
+const val = ref(name);
+</script>
 <template>
-    <input type="text" />
+  <form class="flex relative group">
+    <input
+      :disabled="disabled"
+      v-model="val"
+      :type="type"
+      class="border border-black/60 pl-4 py-2 bg-nud-100 focus:outline-none focus:ring-2 min-w-72 focus:ring-black/60 peer transition-all"
+    />
+    <div
+      :class="
+        val != ''
+          ? '-top-6 pl-0 '
+          : ' inset-0 pl-4 pointer-events-none peer-focus:items-start peer-focus:-top-6 peer-focus:pl-0 peer-focus:text-black/60 '
+      "
+      class="absolute text-black/60 text-xs uppercase flex items-center transition-all"
+    >
+      <slot></slot>
+    </div>
+  </form>
 </template>
